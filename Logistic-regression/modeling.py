dddaddc5a50e51cebfc1529bd8fd910ae1dd90fa
Bhawana Bhatt <75529262+bhawanabhatt538@users.noreply.github.com>
2024-09-16 07:29:40 +0100

    Update modeling.py

--- Logistic-regression/modeling.py
+++ Logistic-regression/modeling.py
@@ -1,1 +1,19 @@
 
+lr = LogisticRegression(random_state=random_state)
+
+param_grid = {
+    'max_iter': [100],
+    'penalty': ['l1', 'l2'],
+    'C': [0.0001, 0.001, 0.01, 0.1, 1, 10],
+    'solver': ['lbfgs', 'liblinear']
+}
+
+lr_clf = GridSearchCV(estimator=lr,
+                      param_grid=param_grid,
+                      scoring=scoring_metric,
+                      cv=5,
+                      verbose=False,
+                      n_jobs=-1)
+
+best_lr_clf = lr_clf.fit(X_train, y_train)
+clf_performance(best_lr_clf, 'Logistic Regression', 'LR')
