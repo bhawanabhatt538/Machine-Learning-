77bf7d317ad8f70cde1efa81161e352fbe36f6b1
Bhawana Bhatt <75529262+bhawanabhatt538@users.noreply.github.com>
2024-09-19 08:16:36 +0100

    Update model_evalution.py

--- model_evalution/model_evalution.py
+++ model_evalution/model_evalution.py
@@ -1,1 +1,9 @@
+from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
 
+rf_Recall = recall_score(y_test, y_pred)
+rf_Precision = precision_score(y_test, y_pred)
+rf_f1 = f1_score(y_test, y_pred)
+rf_accuracy = accuracy_score(y_test, y_pred)
+
+print(cm)
